4297e435e1134f954b6d7fc98bf92c8ced065d54
Diego Rodrigues <diejorarr@gmail.com>
2025-05-07 09:25:08 -0500

    finished game

--- levels/map_manager.gd
+++ levels/map_manager.gd
@@ -2,10 +2,11 @@ extends Node
 
 @export var prev_scene: String
 @export var next_scene: String
+@export var current_scene: String
 
 func _process(_delta):
 	if Input.is_action_pressed("restart"):	
-		get_tree().reload_current_scene()
+		_change_scene_in_place(current_scene)
 	if Input.is_action_pressed("escape"):
 		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 	if Input.is_action_just_pressed("previous_level"):
@@ -20,3 +21,6 @@ func _change_scene_in_place(scene_path: String):
 	
 	to_change.queue_free()
 	parent_to_change.add_child(scene)
+
+func _restart():
+	_change_scene_in_place(current_scene)
--- scripts/death_zone.gd
+++ scripts/death_zone.gd
@@ -3,4 +3,4 @@ extends Area3D
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.name == 'ball':
-		get_tree().reload_current_scene()
+		$"../MapManager"._restart()
